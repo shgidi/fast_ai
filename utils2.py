ed2731a05ee5941ab637f6d65116b9cb288b286d
ubuntu <ubuntu@ps-gpu>
2017-09-26 10:18:34 +0000

    last

--- utils2.py
+++ utils2.py
@@ -18,7 +18,7 @@ from tensorflow.python.framework import ops
 from tensorflow.contrib import rnn, legacy_seq2seq as seq2seq
 
 from keras_tqdm import TQDMNotebookCallback
-from keras import initializations
+#from keras import initializations
 from keras.applications.resnet50 import ResNet50, decode_predictions, conv_block, identity_block
 from keras.applications.vgg16 import VGG16
 from keras.preprocessing import image
